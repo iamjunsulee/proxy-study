5757bcc35979e94daf7d4dc4efe99418a5493043
iamjunsulee <mellow0924@gmail.com>
2022-03-29 11:03:24 +0900

    feat : 구체클래스와 스프링빈 수동등록 버전 추가

--- src/main/java/me/junsu/proxy/ProxyApplication.java
+++ src/main/java/me/junsu/proxy/ProxyApplication.java
@@ -1,11 +1,12 @@
 package me.junsu.proxy;
 
 import me.junsu.proxy.config.AppConfigV1;
+import me.junsu.proxy.config.AppConfigV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppConfigV1.class)
+@Import({AppConfigV1.class, AppConfigV2.class})
 @SpringBootApplication(scanBasePackages = "me.junsu.proxy.app")
 public class ProxyApplication {
 
